743f8088ae77c89a75b19fc8f071c3d96f4fe6a2
niyaaspm <129578665+niyaaspm@users.noreply.github.com>
2023-08-16 14:06:59 +0530

    program completed

---
+++ lib/page1.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:splash_screen/homescreen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(
+      Duration(seconds: 3),
+      () => Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(),
+          )),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("SplashScreen"),
+      ),
+    );
+  }
+}
